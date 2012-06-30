d9eff8eb98549c795e3a12ad238baab92d698b83
N00SA <noosa@comcast.net>
2012-06-29 21:16:25 -0700

    Code alignment; Removed whitespace.

--- makepremiump.php
+++ makepremiump.php
@@ -1,20 +1,19 @@
 <?php
-
-    if(osc_get_preference('allow_premium_plus', 'paypalplus')) {
-        // Load Item Information, so we could tell the user which item is he/she paying for
-        $item = Item::newInstance()->findByPrimaryKey(Params::getParam('itemId'));
-        if($item) {
-            // Check if it's already payed or not
-            if(!paypalplus_is_premium_plus($item['pk_i_id'])) {
-                // Item is not paid, continue
-                $conn = getConnection();
-                $ppl_category = $conn->osc_dbFetchResult("SELECT f_premium_plus_cost FROM %st_paypal_prices WHERE fk_i_category_id = %d", DB_TABLE_PREFIX, $item['fk_i_category_id']);
-                if($ppl_category && isset($ppl_category['f_premium_plus_cost']) && $ppl_category['f_premium_plus_cost']>0) {
-                    $category_fee = $ppl_category['f_premium_plus_cost'];
-                } else {
-                    $category_fee = osc_get_preference('default_premium_plus_cost', 'paypalplus');
-                }
-                if($category_fee > 0) {
+if (osc_get_preference('allow_premium_plus', 'paypalplus')) {
+    // Load Item Information, so we could tell the user which item is he/she paying for
+    $item = Item::newInstance()->findByPrimaryKey(Params::getParam('itemId'));
+    if ($item) {
+        // Check if it's already payed or not
+        if (!paypalplus_is_premium_plus($item['pk_i_id'])) {
+            // Item is not paid, continue
+            $conn = getConnection();
+            $ppl_category = $conn->osc_dbFetchResult("SELECT f_premium_plus_cost FROM %st_paypal_prices WHERE fk_i_category_id = %d", DB_TABLE_PREFIX, $item['fk_i_category_id']);
+            if ($ppl_category && isset($ppl_category['f_premium_plus_cost']) && $ppl_category['f_premium_plus_cost'] > 0) {
+                $category_fee = $ppl_category['f_premium_plus_cost'];
+            } else {
+                $category_fee = osc_get_preference('default_premium_plus_cost', 'paypalplus');
+            }
+            if ($category_fee > 0) {
                 ?>
                 <h1><?php _e('Make the ad premium Plus', 'paypalplus'); ?></h1>
                 <div>
@@ -25,20 +24,22 @@
                     <div style="float:left; width: 50%;">
                         <?php _e("In order to make premium your ad , it's required to pay a fee", 'paypalplus'); ?>.<br/>
                         <?php echo sprintf(__("The current fee for this category is: %.2f %s", 'paypalplus'), $category_fee, osc_get_preference('currency', 'paypalplus')); ?><br/>
-                        <?php if(osc_is_web_user_logged_in()) {
-                                $conn = getConnection();
-                                $wallet = $conn->osc_dbFetchResult("SELECT * FROM %st_paypal_wallet WHERE fk_i_user_id = %d", DB_TABLE_PREFIX, osc_logged_user_id());
-                                $bonuscredit= $conn->osc_dbFetchResult("SELECT * FROM %st_paypal_bonus WHERE fk_i_user_id = %d", DB_TABLE_PREFIX, osc_logged_user_id());
-                                $totalcredit=$wallet['f_amount']+$bonuscredit['f_bonus'];
-							
-								if( $totalcredit>=$category_fee) {
-                                    wallet_button($category_fee, sprintf(__("Premium fee for item %d at %s", "paypalplus"), $item['pk_i_id'], osc_page_title()), $item['fk_i_user_id']."|".$item['pk_i_id']."|".$item['s_contact_email'], "301x".$item['fk_i_category_id']."x".$item['pk_i_id']);
-                                } else {
-                                    paypalplus_button($category_fee, sprintf(__("Premium fee for item %d at %s", "paypalplus"), $item['pk_i_id'], osc_page_title()), $item['fk_i_user_id']."|".$item['pk_i_id']."|".$item['s_contact_email'], "301x".$item['fk_i_category_id']."x".$item['pk_i_id']);
-                                }
+                        <?php
+                        if (osc_is_web_user_logged_in()) {
+                            $conn = getConnection();
+                            $wallet = $conn->osc_dbFetchResult("SELECT * FROM %st_paypal_wallet WHERE fk_i_user_id = %d", DB_TABLE_PREFIX, osc_logged_user_id());
+                            $bonuscredit = $conn->osc_dbFetchResult("SELECT * FROM %st_paypal_bonus WHERE fk_i_user_id = %d", DB_TABLE_PREFIX, osc_logged_user_id());
+                            $totalcredit = $wallet['f_amount'] + $bonuscredit['f_bonus'];
+
+                            if ($totalcredit >= $category_fee) {
+                                wallet_button($category_fee, sprintf(__("Premium fee for item %d at %s", "paypalplus"), $item['pk_i_id'], osc_page_title()), $item['fk_i_user_id'] . "|" . $item['pk_i_id'] . "|" . $item['s_contact_email'], "301x" . $item['fk_i_category_id'] . "x" . $item['pk_i_id']);
                             } else {
-                                paypalplus_button($category_fee, sprintf(__("Premium fee for item %d at %s", "paypalplus"), $item['pk_i_id'], osc_page_title()), $item['fk_i_user_id']."|".$item['pk_i_id']."|".$item['s_contact_email'], "301x".$item['fk_i_category_id']."x".$item['pk_i_id']);
-                            } ?>
+                                paypalplus_button($category_fee, sprintf(__("Premium fee for item %d at %s", "paypalplus"), $item['pk_i_id'], osc_page_title()), $item['fk_i_user_id'] . "|" . $item['pk_i_id'] . "|" . $item['s_contact_email'], "301x" . $item['fk_i_category_id'] . "x" . $item['pk_i_id']);
+                            }
+                        } else {
+                            paypalplus_button($category_fee, sprintf(__("Premium fee for item %d at %s", "paypalplus"), $item['pk_i_id'], osc_page_title()), $item['fk_i_user_id'] . "|" . $item['pk_i_id'] . "|" . $item['s_contact_email'], "301x" . $item['fk_i_category_id'] . "x" . $item['pk_i_id']);
+                        }
+                        ?>
                     </div>
                     <div style="clear:both;"></div>
                     <div name="result_div" id="result_div"></div>
@@ -47,40 +48,39 @@
                     </script>
                 </div>
                 <?php
-                } else {
-                    // PRICE IS ZERO!
-                    ?>
-                    <h1><?php _e('There was an error', 'paypalplus'); ?></h1>
-                    <div>
-                        <p><?php _e("There's no need to pay the premium fee", 'paypalplus'); ?></p>
-                    </div>
-                    <?php
-                }
             } else {
-                // ITEM WAS ALREADY PAID! STOP HERE
+                // PRICE IS ZERO!
                 ?>
                 <h1><?php _e('There was an error', 'paypalplus'); ?></h1>
                 <div>
-                    <p><?php _e('The item is already a premium Plus ad', 'paypalplus'); ?></p>
+                    <p><?php _e("There's no need to pay the premium fee", 'paypalplus'); ?></p>
                 </div>
                 <?php
             }
         } else {
-            //ITEM DOES NOT EXIST! STOP HERE
+            // ITEM WAS ALREADY PAID! STOP HERE
             ?>
             <h1><?php _e('There was an error', 'paypalplus'); ?></h1>
             <div>
-                <p><?php _e('The item doesn not exists', 'paypalplus'); ?></p>
+                <p><?php _e('The item is already a premium Plus ad', 'paypalplus'); ?></p>
             </div>
             <?php
         }
     } else {
-        // NO NEED TO PAY AT ALL!
+        //ITEM DOES NOT EXIST! STOP HERE
         ?>
-        <h1><?php _e("There was an error", "paypalplus");?></h1>
-        <br/>
-        <div><p><?php _e("Premiums Plus ads are not allowed", "paypalplus");?></p></div>
+        <h1><?php _e('There was an error', 'paypalplus'); ?></h1>
+        <div>
+            <p><?php _e('The item doesn not exists', 'paypalplus'); ?></p>
+        </div>
         <?php
     }
-
+} else {
+    // NO NEED TO PAY AT ALL!
+    ?>
+    <h1><?php _e("There was an error", "paypalplus"); ?></h1>
+    <br/>
+    <div><p><?php _e("Premiums Plus ads are not allowed", "paypalplus"); ?></p></div>
+    <?php
+}
 ?>
