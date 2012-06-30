b1e604161e6918908ae739e903e6598e7860d0a2
N00SA <noosa@comcast.net>
2012-06-29 20:23:46 -0700

    Code alignment; Removed whitespace after closing tag to prevent 'header already sent' error.

--- upgrade.php
+++ upgrade.php
@@ -1,29 +1,25 @@
 <?php
 /*
-Plugin Name: PaypalPlus payment
-Plugin URI: http://www.osclass.org/
-Description: Paypal payment options
-Version: 2.0.3
-Author: OSClass . New features by Cris
-Author URI: http://www.osclass.org/
-Short Name: paypalplus
-*/
+  Plugin Name: PaypalPlus payment
+  Plugin URI: http://www.osclass.org/
+  Description: Paypal payment options
+  Version: 2.0.3
+  Author: OSClass . New features by Cris
+  Author URI: http://www.osclass.org/
+  Short Name: paypalplus
+ */
 
+// load necessary functions
+$conn = getConnection();
+$path = osc_plugin_resource('paypalplus/struct.sql');
+$sql = file_get_contents($path);
+$conn->osc_dbImportSQL($sql);
 
-    // load necessary functions
+osc_set_preference('allow_premium_plus', '0', 'paypal', 'BOOLEAN');
+$conn->autocommit(true);
+echo "done";
+Plugins::runHook('paypal_uninstall');
+Plugins::uninstall('paypal');
 
-  
-	    $conn = getConnection() ;
-        $path = osc_plugin_resource('paypalplus/struct.sql');
-        $sql  = file_get_contents($path);
-        $conn->osc_dbImportSQL($sql);
-
-        
-        osc_set_preference('allow_premium_plus', '0', 'paypal', 'BOOLEAN');
-		$conn->autocommit(true);
-		echo "done";
-  		 Plugins::runHook('paypal_uninstall') ;
-                    Plugins::uninstall('paypal');
-
-                    osc_add_flash_ok_message( _m('Plugin uninstalled'), 'admin');
+osc_add_flash_ok_message(_m('Plugin uninstalled'), 'admin');
 ?>
